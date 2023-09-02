295f4fb9228ae9ccebdb93609c55689d592a5652
iansantos16 <143853212+iansantos16@users.noreply.github.com>
2023-09-01 23:51:50 -0400

    Add files via upload

---
+++ truthy-falsy.js
@@ -0,0 +1,26 @@
+const usuarioLogado = true
+const contapaga = false
+
+// truthy or falsy
+
+// 0 false     1 true
+
+console.log(0 == false);
+console.log(''== false);
+console.log(1 == true);
+
+// null vazio
+// undefined
+
+let minhaVar
+let varNull = null
+
+console.log(minhaVar)
+console.log(varNull)
+
+
+let numero =3
+let texto ='alura'
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
---
+++ type-string.js
@@ -0,0 +1,12 @@
+const texto1 = 'Ola mundo';
+const citacao = 'meu nome é'
+
+const meuNome = ian
+
+// concatenacao
+
+console.log(citacao+meuNome)
+
+// template string ou lateral
+
+
